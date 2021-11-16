e3604e804b05d5164eb923407b0e40ad89a4577e
DimaAmega <dima.amega1@gmail.com>
2021-11-16 09:26:22 +0300

    fill git-assets-flow

--- git-assets-flow.sh
+++ git-assets-flow.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -e
 
+if [ $# -eq 0 ]
+  then
+    echo "The commit message is not specified"
+    exit 1
+fi
+
+NODE_ENV=production npm run gulp-assets
 git checkout gh-pages
+ls | grep -v dist | xargs rm -rf
+cp dist/* .
+rm -rf dist
+git add .
+git commit -m "$1"
+git checkout main
+npm i

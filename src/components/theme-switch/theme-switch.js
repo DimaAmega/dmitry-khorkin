86cbaec17c74dae479bd5654687a313534976668
DimaAmega <dima.amega1@gmail.com>
2021-12-06 12:07:11 +0300

    persistent theme-switch

--- src/components/theme-switch/theme-switch.js
+++ src/components/theme-switch/theme-switch.js
@@ -1,11 +1,29 @@
-const _root = document.documentElement;
-const _theme_switch = document.querySelector("#theme-switch");
-let { matches: isDark } = window.matchMedia("(prefers-color-scheme: dark)");
+const root = document.documentElement;
+const theme_switch = document.querySelector("#theme-switch");
+const lsGet = (p) => localStorage.getItem(p);
+const lsSet = (k, v) => localStorage.setItem(k, v);
 
-_theme_switch.querySelector("input").checked = isDark;
+let isDark = lsGet("isDark");
 
-_theme_switch.addEventListener("change", () => {
+if (isDark == "true") isDark = true;
+if (isDark == "false") isDark = false;
+if (isDark == null)
+  // don't set
+  isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const showState = (isDark) => {
+  root.classList.toggle("dark-theme", isDark);
+  root.classList.toggle("light-theme", !isDark);
+  lsSet("isDark", isDark);
+};
+
+const changeHandler = () => {
   isDark = !isDark;
-  _root.classList.toggle("dark-theme", isDark);
-  _root.classList.toggle("light-theme", !isDark);
-});
+  showState(isDark);
+};
+
+theme_switch.addEventListener("change", changeHandler);
+
+/* MAIN */
+theme_switch.querySelector("input").checked = isDark;
+showState(isDark);
